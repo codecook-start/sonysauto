386326f5a46b8dbf2de38cbff034efaa321eb06e
Gaurav Gupta <gauravmaharishi6392@gmail.com>
2025-01-25 17:34:13 +0530

    add filter as in the reference

--- components/CarListing/ResultFilter.tsx
+++ components/CarListing/ResultFilter.tsx
@@ -1,15 +1,10 @@
-import { Button } from "@/components/ui/button";
 import { useCars } from "@/hooks/useCars";
-import { useFilter } from "@/hooks/useFilter";
 import { carPaginationAtom } from "@/jotai/carsAtom";
 import { useAtom } from "jotai";
 import { X } from "lucide-react";
-import React from "react";
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 const ResultFilter = () => {
   const { refetch } = useCars();
-  const { resetAll } = useFilter();
   const [pagination, setPagination] = useAtom(carPaginationAtom);
   const handleFilterChange = async (name: string, values: string[]) => {
     setPagination((prev) => {
@@ -25,28 +20,19 @@ const ResultFilter = () => {
     await refetch();
   };
 
-  const isFilterActive = pagination.details.some(
-    (detail) => detail.values.length > 0,
-  );
-
   return (
-    <div className="results space-x-1">
+    <div className="results mb-4 mt-4 space-x-1">
       {/* show all the filters */}
       <div className="filters mt-2 flex gap-2">
-        {isFilterActive && (
-          <Button onClick={resetAll} className="rounded-full text-xs">
-            Clear All
-          </Button>
-        )}
-        <ScrollArea>
-          <div className="flex h-full gap-2">
+        <div className="filters mt-2 flex flex-wrap items-center gap-2">
+          <div className="flex h-full flex-wrap gap-2">
             {pagination.details.map((detail, i) =>
               detail.values.map((value, j) => (
                 <div
                   key={`${i}-${j}`}
-                  className="flex items-center rounded-full bg-gray-200 px-4 py-2"
+                  className="flex items-center rounded-full bg-gray-200 px-2 py-1"
                 >
-                  <span className="text-xs capitalize">{detail.name}:</span>
+                  {/* <span className="text-xs capitalize">{detail.name}:</span> */}
                   <span className="ml-1 text-xs">{value}</span>
                   <X
                     onClick={() =>
@@ -61,8 +47,7 @@ const ResultFilter = () => {
               )),
             )}
           </div>
-          <ScrollBar orientation="horizontal" className="opacity-80" />
-        </ScrollArea>
+        </div>
       </div>
     </div>
   );
